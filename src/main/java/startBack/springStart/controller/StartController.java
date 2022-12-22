08ac2eda66b4979550abfaa347ff53b03a3cdc97
wnstn819 <wnstn819@naver.com>
2022-12-22 18:05:47 +0900

    Get 데이터 넘기기

--- src/main/java/startBack/springStart/controller/StartController.java
+++ src/main/java/startBack/springStart/controller/StartController.java
@@ -3,6 +3,7 @@ package startBack.springStart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StartController {
@@ -13,5 +14,12 @@ public class StartController {
         model.addAttribute("data", "start!");
         model.addAttribute("data2","welcom!");
         return "start";
+    }
+
+    @GetMapping("start-mvc")
+    public String startMvc(@RequestParam(value = "name",required = true) String name, Model model){
+        model.addAttribute("name",name);
+        return "start-template";
+
     }
 }
