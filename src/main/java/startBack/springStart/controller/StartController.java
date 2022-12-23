b2206574239d2fb2f5f82218ba1de802be7c90e1
wnstn819 <wnstn819@naver.com>
2022-12-23 19:56:03 +0900

    API 예제

--- src/main/java/startBack/springStart/controller/StartController.java
+++ src/main/java/startBack/springStart/controller/StartController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StartController {
@@ -20,6 +21,31 @@ public class StartController {
     public String startMvc(@RequestParam(value = "name",required = true) String name, Model model){
         model.addAttribute("name",name);
         return "start-template";
+
+    }
+
+    @GetMapping("start-string")
+    @ResponseBody
+    public String startStrnig(@RequestParam("name") String name){
+        return "start" + name;
+    }
+
+    @GetMapping("start-api")
+    @ResponseBody
+    public Start startApi(@RequestParam("name") String name){
+        Start start = new Start();
+        start.setName(name);
+        return start;
+    }
+    static class Start{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
 
     }
 }
