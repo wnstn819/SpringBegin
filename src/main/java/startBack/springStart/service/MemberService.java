4f286b3076b08d974b48e827812c9b25925eabef
wnstn819 <wnstn819@naver.com>
2022-12-24 16:11:08 +0900

    회원 서비스 테스트

---
+++ src/main/java/startBack/springStart/service/MemberService.java
@@ -0,0 +1,49 @@
+package startBack.springStart.service;
+
+import startBack.springStart.domain.Member;
+import startBack.springStart.repository.MemberRepository;
+import startBack.springStart.repository.MemoryMemberRepository;
+
+import java.util.*;
+
+public class MemberService {
+
+    //private final MemberRepository memberRepository = new MemberRepository(); -> 이렇게 하면 의존성 주입이 안됨
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     *
+     * 회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+           throw new IllegalStateException("이미 존재하는 회원입니다.");
+       });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMembers(){
+       return  memberRepository.findAll();
+
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
